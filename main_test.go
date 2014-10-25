5505a992d9054c26ac45efe382607c1cb4d56276
Pirmin Fix <pirmin.fix@jimdo.com>
2014-10-25 18:18:49 +0200

    add potential new test

--- main_test.go
+++ main_test.go
@@ -4,9 +4,17 @@ import (
 	"testing"
 )
 
-func TestSdlError(t *testing.T) {
+func TestPlainSDLError(t *testing.T) {
 	err := NewSdlError("wurst")
 	if err.Error() != "wurst" {
 		t.Errorf("%v != %v", err.Error(), "wurst")
 	}
+}
+
+func TestRealSDLError(t *testing.T) {
+	// TODO:
+	// We need to create a real sdl error
+	// then create a custom sdl error and see
+	// what happens
+	t.SkipNow()
 }
