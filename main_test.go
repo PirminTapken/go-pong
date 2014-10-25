8568ef4d77bf0e2a3ac7206cbc2a8ea9385e7659
Pirmin Fix <pirmin.fix@jimdo.com>
2014-10-26 00:07:02 +0200

    add test for background fill

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 func TestPlainSDLError(t *testing.T) {
@@ -17,4 +19,35 @@ func TestRealSDLError(t *testing.T) {
 	// then create a custom sdl error and see
 	// what happens
 	t.SkipNow()
+}
+
+// We want to know if the background is really filled with the background color
+func TestBackgroundFill(t *testing.T) {
+	size := struct {
+		W int
+		H int
+	}{
+		10,
+		10,
+	}
+	window, renderer := sdl.CreateWindowAndRenderer(size.W, size.H, 0)
+	err := fillBackground(window, renderer)
+	if err != nil {
+		t.Error("fill background error", err)
+	}
+
+	surface := window.GetSurface()
+	pixels := surface.Pixels()
+	firstPixel := uint32(pixels[0])<<8 + uint32(pixels[1])<<8 + uint32(pixels[2])<<8 + uint32(pixels[3])
+
+	if firstPixel != uint32(BACKGROUND) {
+		t.Error("FirstPixel !=", BACKGROUND)
+	}
+
+	lastPixel := uint32(pixels[surface.PixelNum()-3])<<8 + uint32(pixels[surface.PixelNum()-2])<<8 + uint32(pixels[surface.PixelNum()-1])<<8 + uint32(pixels[surface.PixelNum()])
+
+	if lastPixel != uint32(BACKGROUND) {
+		t.Error("LastPixel !=", BACKGROUND)
+	}
+
 }
