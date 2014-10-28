9c17f5a5272822365f361294da423555c124d51b
Pirmin Fix <pirmin.fix@jimdo.com>
2014-10-28 12:39:32 +0100

    start of effort

--- main.go
+++ main.go
@@ -8,268 +8,94 @@ import (
 )
 
 const (
-	BACKGROUND    = uint32(0x000000)
-	PADDEL_LENGTH = 100
-	PADDEL_WIDTH  = 20
-	PADDEL_COLOR  = uint32(0xffffff)
+	BACKGROUND_COLOR = uint32(0x000000)
+	PADDEL_LENGTH    = 100
+	PADDEL_WIDTH     = 20
+	PADDEL_COLOR     = uint32(0xffffff)
+	SCREEN_WIDTH     = 640
+	SCREEN_HEIGHT    = 480
+	NAME             = "Pong"
 )
 
-type SdlError struct {
-	msg      string
-	sdlError error
+type PongError struct {
+	SDLError error
+	Msg      string
 }
 
-func NewSdlError(msg string) *SdlError {
-	return &SdlError{
-		msg:      msg,
-		sdlError: sdl.GetError(),
+func NewPongError(msg string) *PongError {
+	e := &PongError{
+		SDLError: sdl.GetError(),
+		Msg:      msg,
 	}
+	return e
 }
 
-func (e *SdlError) Error() string {
-	if e.sdlError == nil {
-		return e.msg
-	} else {
-		return strings.Join(
-			[]string{
-				e.msg,
-				e.sdlError.Error(),
-			},
-			": ",
-		)
+func (e *PongError) Error() string {
+	// we don't need do much if we don't have sdl errors present
+	if e.SDLError == nil {
+		return e.Msg
 	}
+	errMsg := strings.Join(
+		[]string{
+			e.Msg,
+			e.SDLError.Error(),
+		},
+		": ",
+	)
+	return errMsg
 }
 
-func fillBackground(window *sdl.Window, renderer *sdl.Renderer) error {
-	w, h := window.GetSize()
+// run on the renderer
+func Run(
+	screen struct {
+		W int
+		H int
+	},
+	renderer *sdl.Renderer,
+) error {
 	background := sdl.CreateRGBSurface(
 		0,
-		int32(w),
-		int32(h),
+		int32(screen.W),
+		int32(screen.H),
 		32,
 		0, 0, 0, 0,
 	)
 	if background == nil {
-		return NewSdlError("background nil")
-	}
-	allRect := &sdl.Rect{
-		W: int32(w),
-		H: int32(h),
-		X: 0,
-		Y: 0,
+		return NewPongError("background creation failed")
 	}
 	if background.FillRect(
-		allRect,
-		BACKGROUND,
-	) != 0 {
-		return NewSdlError("Background fill failed")
-	}
-	tex := renderer.CreateTextureFromSurface(background)
-	if tex == nil {
-		return NewSdlError("CreateTextureFromSurfaceFailed")
-	}
-	background.Free()
-	if renderer.Copy(tex, nil, allRect) != 0 {
-		return NewSdlError("RenderCopy Failed")
-	}
-	tex.Destroy()
-	return nil
-}
-
-type EventListener interface {
-	PutEvent(event sdl.Event)
-}
-
-// Right now this type needs to be created manually
-// I am too lazy now to write a registrar for new
-// listener
-type EventDistributor struct {
-	Listener []EventListener
-}
-
-// let this event distributor run in own goroutine
-func (ed *EventDistributor) Run() {
-	var event sdl.Event
-	for {
-		event = sdl.PollEvent()
-		if event != nil {
-			for _, listener := range ed.Listener {
-				go listener.PutEvent(event)
-			}
-		}
-	}
-}
-
-type Coord struct {
-	X uint
-	Y uint
-}
-
-type Paddel struct {
-	coord        Coord
-	surface      *sdl.Surface
-	errChan      chan error
-	coordChan    chan Coord
-	newCoordChan chan Coord
-}
-
-func NewPaddel(initialX, initialY uint) (*Paddel, error) {
-	p := Paddel{
-		coord:        Coord{X: initialX, Y: initialY},
-		surface:      sdl.CreateRGBSurface(0, int32(PADDEL_WIDTH), int32(PADDEL_LENGTH), 32, 0, 0, 0, 0),
-		errChan:      make(chan error),
-		coordChan:    make(chan Coord),
-		newCoordChan: make(chan Coord),
-	}
-	if p.surface == nil {
-		return nil, NewSdlError("Creating Surface for new Paddel failed")
-	}
-	if p.surface.FillRect(
 		&sdl.Rect{
-			X: 0, Y: 0,
-			W: PADDEL_WIDTH,
-			H: PADDEL_LENGTH,
+			X: 0,
+			Y: 0,
+			W: int32(screen.W),
+			H: int32(screen.H),
 		},
-		PADDEL_COLOR,
+		BACKGROUND_COLOR,
 	) != 0 {
-		return nil, NewSdlError("NewPaddel Surface filling failed")
+		return NewPongError("Filling background failed")
 	}
-	go p.run()
-	return &p, nil
-}
-
-func (p *Paddel) PutEvent(evt sdl.Event) {
-	switch t := evt.(type) {
-	case *sdl.KeyDownEvent:
-		switch t.Keysym.Sym {
-		case sdl.K_UP:
-			log.Print("up event")
-			p.MoveUp(10)
-		case sdl.K_DOWN:
-			log.Print("down event")
-			p.MoveDown(10)
-		}
-	case *sdl.QuitEvent:
-		// FIXME how to quit all this?
-		log.Print("QuitEvent")
+	backgroundTexture := renderer.CreateTextureFromSurface(background)
+	if backgroundTexture == nil {
+		return NewPongError("Creating Background Texture failed")
 	}
-}
-
-func (p *Paddel) GetCoord() (Coord, error) {
-	// Fixme: make this faster
-	return <-p.coordChan, nil
-}
-
-func (p *Paddel) Render(renderer *sdl.Renderer) error {
-	tex := renderer.CreateTextureFromSurface(p.surface)
-	if tex == nil {
-		return NewSdlError("Creating Texture for Paddel render failed")
-	}
-	coord, err := p.GetCoord()
-	if err != nil {
-		return err
-	}
-	if renderer.Copy(tex, nil, &sdl.Rect{
-		X: int32(coord.X),
-		Y: int32(coord.Y),
-		W: PADDEL_WIDTH,
-		H: PADDEL_LENGTH,
-	}) != 0 {
-		return NewSdlError("Render Copy for Paddel failed")
-	}
-	return nil
-}
-
-// never call this!
-func (p *Paddel) setCoords(newCoords Coord) {
-	p.coord = newCoords
-}
-
-// internal runner
-func (p *Paddel) run() {
-	for {
-		select {
-		case newCoords := <-p.newCoordChan:
-			p.setCoords(newCoords)
-		case p.coordChan <- p.coord:
-			// We just wanted to answer a request
-		default:
-			continue
-		}
-	}
-}
-
-func (p *Paddel) MoveDown(i uint) {
-	coord, _ := p.GetCoord()
-	coord.Y = coord.Y + i
-	p.newCoordChan <- coord
-}
-
-func (p *Paddel) MoveUp(i uint) {
-	coord, _ := p.GetCoord()
-	if coord.Y < i {
-		coord.Y = 0
-	} else {
-		coord.Y = coord.Y - i
-	}
-	p.newCoordChan <- coord
-}
-
-// Run the program after all init stuff
-func Run(window *sdl.Window, renderer *sdl.Renderer) error {
-	log.Print("starting run")
-	var err error
-	w, h := window.GetSize()
-	leftPaddel, err := NewPaddel(
-		10,
-		uint((h-PADDEL_LENGTH)/2),
-	)
-	if err != nil {
-		return err
-	}
-	rightPaddel, err := NewPaddel(
-		uint(w-10-PADDEL_WIDTH),
-		uint((h-PADDEL_LENGTH)/2),
-	)
-	if err != nil {
-		return err
-	}
-
-	eventDistributor := &EventDistributor{
-		Listener: []EventListener{leftPaddel, rightPaddel},
-	}
-
-	go eventDistributor.Run()
-
-	// main loop
-	for {
-		err = fillBackground(window, renderer)
-		if err != nil {
-			return err
-		}
-		for _, p := range []*Paddel{leftPaddel, rightPaddel} {
-			err = p.Render(renderer)
-			if err != nil {
-				return err
-			}
-		}
-		renderer.Present()
+	// copy whole texture to whole target
+	if renderer.Copy(backgroundTexture, nil, nil) != 0 {
+		return NewPongError("Copying Background Texture failed")
 	}
 	return nil
 }
 
 func main() {
-	log.Print("Hello World!")
 	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
 		log.Fatal(sdl.GetError())
 	}
 	defer sdl.Quit()
 	window := sdl.CreateWindow(
-		"Pong",
+		NAME,
 		sdl.WINDOWPOS_UNDEFINED,
 		sdl.WINDOWPOS_UNDEFINED,
-		640,
-		480,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
 		0,
 	)
 	if window == nil {
@@ -285,7 +111,10 @@ func main() {
 		log.Fatal(sdl.GetError(), "creating renderer failed")
 	}
 	defer renderer.Destroy()
-	err := Run(window, renderer)
+	err := Run(struct {
+		W int
+		H int
+	}{SCREEN_WIDTH, SCREEN_HEIGHT}, renderer)
 	if err != nil {
 		log.Fatal(err)
 	}
