d4bd862832acc2719156aa6cf8213a7c5cc163a5
Pirmin Tapken <pirmin.fix@jimdo.com>
2014-12-20 22:36:12 +0100

    adding vector lib

---
+++ vector.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"math"
+)
+
+// no inplace operations
+type Vector2d [2]float64
+
+// Add vector w to v and return result
+func (v *Vector2d) Add(w *Vector2d) *Vector2d {
+	u := new(Vector2d)
+	for i, _ := range v {
+		u[i] = v[i] + w[i]
+	}
+	return u
+}
+
+// Scale vector by factor f
+func (v *Vector2d) Scale(f float64) *Vector2d {
+	u := new(Vector2d)
+	for i, _ := range v {
+		u[i] = v[i] * f
+	}
+	return u
+}
+
+func (v *Vector2d) Dot(w *Vector2d) float64 {
+	return v[0]*w[0] + v[1]*w[1]
+}
+
+func (v *Vector2d) Sub(w *Vector2d) *Vector2d {
+	return v.Add(w.Scale(-1.0))
+}
+
+func (v *Vector2d) Len() float64 {
+	return math.Sqrt(v.Dot(v))
+}
+
+type Line [2]*Vector2d
+
+// get the factor for intersection with line k
+func (l *Line) Intersect(k *Line) float64 {
+	// see http://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+	// for the calculation
+
+	E := l[1].Sub(l[0])
+	F := k[1].Sub(k[0])
+	P := &Vector2d{-1 * E[1], E[0]}
+	h := l[1].Sub(k[1]).Dot(P) / F.Dot(P)
+	return h
+}
+
+func (l *Line) Vector2d() *Vector2d {
+	return l[0].Add(l[1])
+}
