064066e74ce0bce2f13fb34c108b361c96d929ab
Pirmin Tapken <pirmin.fix@jimdo.com>
2015-01-27 07:30:21 +0100

    Create awesome key event subscriber thing

--- events.go
+++ events.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	//	"fmt"
 	"github.com/veandco/go-sdl2/sdl"
 	"time"
 )
@@ -20,53 +19,94 @@ func (ses *SdlEventStream) Close() error {
 func (ses *SdlEventStream) loop() {
 	var err error
 	for {
-		//evt := sdl.PollEvent()
+		evt := sdl.PollEvent()
 		select {
 		case errc := <-ses.closing:
 			errc <- err
 			close(ses.events)
 			return
 		case <-time.After(1 * time.Second):
+		default:
+			if evt != nil {
+				ses.events <- evt
+			}
 		}
 	}
 }
 
-func (ses *SdlEventStream) GetEvents() <-chan sdl.Event {
-	ses.events = make(chan sdl.Event)
-	ses.closing = make(chan chan error)
-	go ses.loop()
+// Receive returns a read only channel where sdl Events will
+// be submitted
+func (ses *SdlEventStream) Receive() <-chan sdl.Event {
+	// This makes it possible to call this more than once
+	// but don't have this starting two loops or more
+	if ses.events == nil {
+		ses.events = make(chan sdl.Event)
+		ses.closing = make(chan chan error)
+		go ses.loop()
+	}
 	return ses.events
 }
 
+type subscribeMessage struct {
+	Key  sdl.Keycode
+	Chan chan sdl.Event
+}
+
 // KeyEventSubscriber sfilters key events from an event stream
-type KeyEventSubscriber struct {
-	eventStream <-chan sdl.Event
-	closing     chan chan error
+type EventSubscriber struct {
+	eventStream   *SdlEventStream
+	eventChan     <-chan sdl.Event
+	closing       chan chan error
+	recepients    map[sdl.Keycode][]chan sdl.Event
+	newSubscriber chan subscribeMessage
 }
 
 // NewKeyFilter returns an initialized KeyFilter
-func NewKeyEventSubscriber(eventStream <-chan sdl.Event) *KeyEventSubscriber {
-	kes := &KeyEventSubscriber{eventStream, make(chan chan error)}
-	go kes.loop()
-	return kes
+func NewEventSubscriber() *EventSubscriber {
+	es := &EventSubscriber{}
+	es.eventStream = &SdlEventStream{}
+	es.eventChan = es.eventStream.Receive()
+	es.recepients = make(map[sdl.Keycode][]chan sdl.Event)
+	es.closing = make(chan chan error)
+	es.newSubscriber = make(chan subscribeMessage)
+	go es.loop()
+	return es
 }
 
-func (kes *KeyEventSubscriber) loop() {
+func (es *EventSubscriber) loop() {
 	var err error
 	for {
 		select {
-		case errc := <-kes.closing:
+		case errc := <-es.closing:
 			errc <- err
 			return
-		case <-kes.eventStream:
-			// distribute
+		case evt := <-es.eventChan:
+			switch et := evt.(type) {
+			case *sdl.KeyDownEvent:
+				for _, c := range es.recepients[et.Keysym.Sym] {
+					c <- evt
+				}
+			}
+		case sub := <-es.newSubscriber:
+			if es.recepients[sub.Key] == nil {
+				es.recepients[sub.Key] = []chan sdl.Event{sub.Chan}
+			} else {
+				es.recepients[sub.Key] = append(es.recepients[sub.Key], sub.Chan)
+			}
 		}
 	}
 }
 
-func (kes *KeyEventSubscriber) KeyEvents(sdl.Keycode) <-chan sdl.Event {
-	// FIXME
-	return kes.eventStream
+func (es *EventSubscriber) Subscribe(key sdl.Keycode) <-chan sdl.Event {
+	c := make(chan sdl.Event)
+	es.newSubscriber <- subscribeMessage{key, c}
+	return c
+}
+
+func (es *EventSubscriber) Close() error {
+	errc := make(chan error)
+	es.closing <- errc
+	return <-errc
 }
 
 // KeyDownEventHandler tackles KeyDown Events and executes action on correct key
--- events_test.go
+++ events_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/sdl"
 	"testing"
 	"time"
 )
@@ -9,7 +9,7 @@ import (
 // TestCloseEventStream tests if closing our event stream works
 func TestCloseEventStream(t *testing.T) {
 	es := &SdlEventStream{}
-	events := es.GetEvents()
+	events := es.Receive()
 	t.Log("Opened channel, calling close in background")
 	errc := make(chan error)
 	// do it in background to give it time
@@ -30,6 +30,60 @@ func TestCloseEventStream(t *testing.T) {
 	}
 }
 
+func TestCloseEventSubscriber(t *testing.T) {
+	es := NewEventSubscriber()
+	errc := make(chan error)
+	go func() {
+		errc <- es.Close()
+	}()
+	select {
+	case err := <-errc:
+		if err != nil {
+			t.Errorf("closing returned error: %v", err)
+		}
+	case <-time.After(3 * time.Second):
+		t.Errorf("timeout!")
+	}
+}
+
 func TestReceiveSpecificKeyEvent(t *testing.T) {
-	t.Error("not implemented")
+	done := make(chan bool)
+	go func() {
+		es := NewEventSubscriber()
+		t.Log("Subscribe")
+		evtChan := es.Subscribe(sdl.K_UP)
+		t.Log("Create Fake Event")
+		fakeEvent := &sdl.KeyDownEvent{
+			Keysym: sdl.Keysym{
+				Sym:      sdl.K_UP,
+				Scancode: sdl.SCANCODE_UP,
+				Mod:      sdl.KMOD_NONE,
+				Unicode:  0,
+			},
+			Type:      sdl.KEYDOWN,
+			Timestamp: 0,
+			WindowID:  0,
+			State:     sdl.PRESSED,
+			Repeat:    0,
+		}
+		t.Log("Push fake Event")
+		sdl.PushEvent(fakeEvent)
+		select {
+		case evt := <-evtChan:
+			switch e := evt.(type) {
+			case *sdl.KeyDownEvent:
+				if e.Keysym.Sym != sdl.K_UP {
+					t.Errorf("Key is %v instead of %v", e.Keysym.Sym, sdl.K_UP)
+				}
+			}
+		case <-time.After(5 * time.Second):
+			t.Error("timeout after 5 seconds")
+		}
+		done <- true
+	}()
+	select {
+	case <-done:
+	case <-time.After(10 * time.Second):
+		t.Error("Timeout Nothing went well")
+	}
 }
