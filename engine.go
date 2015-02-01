10fbe8e8b3319fddb4c9cac64f2f69b4bd77169f
Pirmin Tapken <pirmin.fix@jimdo.com>
2015-02-01 18:39:24 +0100

    Expand upon Engine
    Now it really creates window and stuff

--- engine.go
+++ engine.go
@@ -1,6 +1,8 @@
 package main
 
-import ()
+import (
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 const (
 	BACKGROUND_COLOR = uint32(0x000000)
@@ -11,6 +13,9 @@ const (
 
 // Engine is our little nice graphics engine
 type Engine struct {
+	cleanupFns []func()
+	window     *sdl.Window
+	renderer   *sdl.Renderer
 	// Thread is public so it can be used by other
 	// goroutines that need to do stuff in the sdl
 	// thread
@@ -21,6 +26,7 @@ type Engine struct {
 // error is always nil and just there to match
 // io.Closer
 func (e *Engine) Close() error {
+	e.cleanup()
 	err := e.Thread.Close()
 	return err
 }
@@ -28,6 +34,70 @@ func (e *Engine) Close() error {
 // NewEngine creates the engine.
 // This basically creates the background texture and stores it away
 func NewEngine(windowName string, X, Y, W, H int) (e *Engine, err error) {
-	e = &Engine{Thread: NewThread()}
-	return e, nil
+	e = &Engine{Thread: NewThread(), cleanupFns: make([]func(), 0)}
+	err = e.sdlInit()
+	if err != nil {
+		return e, err
+	}
+	err = e.CreateWindowAndRenderer(W, H, 0)
+	e.SetTitle(windowName)
+	return e, err
+}
+
+func (e *Engine) Title() string {
+	return e.Thread.Exec(func() interface{} {
+		return e.window.GetTitle()
+	}).(string)
+}
+
+func (e *Engine) SetTitle(s string) {
+	_ = e.Thread.Exec(func() interface{} {
+		e.window.SetTitle(s)
+		return nil
+	})
+}
+
+// init calls sdl init in sdl thread
+func (e *Engine) sdlInit() error {
+	r := e.Thread.Exec(func() interface{} {
+		// This is neccessary otherwise nil error can't be converted to
+		// interface and back somehow...
+		e := struct{ err error }{}
+		if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
+			e.err = sdl.GetError()
+		}
+		return e
+	}).(struct{ err error })
+	e.cleanupFns = append(e.cleanupFns, sdl.Quit)
+	return r.err
+}
+
+// cleanup cleans everything up
+func (e *Engine) cleanup() {
+	for i := len(e.cleanupFns); i > 0; i-- {
+		e.Thread.Exec(func() interface{} {
+			e.cleanupFns[i-1]()
+			return nil
+		})
+	}
+}
+
+func (e *Engine) CreateWindowAndRenderer(w, h int, flags uint32) error {
+	type resp struct {
+		w *sdl.Window
+		r *sdl.Renderer
+		e error
+	}
+	r := e.Thread.Exec(func() interface{} {
+		w, r, e := sdl.CreateWindowAndRenderer(w, h, flags)
+		return resp{w: w, r: r, e: e}
+	}).(resp)
+	if r.e != nil {
+		return r.e
+	}
+	e.window = r.w
+	e.renderer = r.r
+	e.cleanupFns = append(e.cleanupFns, e.window.Destroy)
+	e.cleanupFns = append(e.cleanupFns, e.renderer.Destroy)
+	return nil
 }
--- engine_test.go
+++ engine_test.go
@@ -4,15 +4,33 @@ import (
 	"testing"
 )
 
-func TestEngineClose(t *testing.T) {
-	e, err := NewEngine("test", 100, 100, 100, 100)
+func getNewEngine(t *testing.T, s string, a, b, c, d int) (e *Engine) {
+	e, err := NewEngine(s, a, b, c, d)
 	if err != nil {
 		t.Error(err)
 	}
 	if e == nil {
 		t.Error("Engine is nil")
 	}
-	err = e.Close()
+	return
+}
+
+func TestEngineClose(t *testing.T) {
+	e := getNewEngine(t, "test", 100, 100, 100, 100)
+	err := e.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestEngineSetTitle(t *testing.T) {
+	s := "TestTitle"
+	e := getNewEngine(t, "test", 100, 100, 100, 100)
+	e.SetTitle(s)
+	if e.Title() != s {
+		t.Error("setting title failed")
+	}
+	err := e.Close()
 	if err != nil {
 		t.Error(err)
 	}
