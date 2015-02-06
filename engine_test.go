3eb74245612a2370098cfe39a492329f3ed247fe
Pirmin Fix <ookami.gnu@gmail.com>
2015-02-06 23:19:04 +0100

    Scenegraph tests

--- engine_test.go
+++ engine_test.go
@@ -35,3 +35,31 @@ func TestEngineSetTitle(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestAppendChild(t *testing.T) {
+	r := new(Node)
+	n := []*Node{new(Node), new(Node), new(Node)}
+	for _, c := range n {
+		r.AppendChild(c)
+	}
+	if n[0] != r.FirstChild ||
+		n[1] != n[0].NextSibling ||
+		n[1] != n[2].PrevSibling ||
+		n[2] != r.LastChild ||
+		n[1].Parent != r {
+		t.Error("Fail")
+	}
+}
+
+func TestRemoveChild(t *testing.T) {
+	r := new(Node)
+	n := []*Node{new(Node), new(Node), new(Node)}
+	for _, c := range n {
+		r.AppendChild(c)
+	}
+	r.RemoveChild(n[1])
+	if n[0].NextSibling != n[2] ||
+		n[2].PrevSibling != n[0] {
+		t.Error("Fail")
+	}
+}
